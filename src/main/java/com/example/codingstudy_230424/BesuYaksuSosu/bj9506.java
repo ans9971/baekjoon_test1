bda22e1c6e38abd230cedfaa7dfc46f6d6f19910
ans9971 <ans9971@naver.com>
2023-06-24 23:58:51 +0900

    .

--- src/main/java/com/example/codingstudy_230424/BesuYaksuSosu/bj9506.java
+++ src/main/java/com/example/codingstudy_230424/BesuYaksuSosu/bj9506.java
@@ -7,7 +7,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.*;
 
-interface bj9506 {
+public class bj9506 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
