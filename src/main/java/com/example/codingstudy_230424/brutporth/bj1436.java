cc272877bed7f57815db2b6ca08b1bb895ab2094
ans9971 <ans9971@naver.com>
2023-06-27 23:31:42 +0900

    .

---
+++ src/main/java/com/example/codingstudy_230424/brutporth/bj1436.java
@@ -0,0 +1,26 @@
+package com.example.codingstudy_230424.brutporth;
+import java.util.Scanner;
+
+public class bj1436 {
+    public static void main(String[] args)   {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt(); // nth count
+        scan.close();
+        int cnt = 0; //count
+        int asw = 660; // answer
+        while(n > cnt){
+            asw++;
+            int temp = asw;
+            while (temp>=666) {
+                if(temp%1000 == 666){
+                    cnt++;
+                    break;
+                }
+                else{
+                    temp/=10;
+                }
+            }
+        }
+        System.out.println(asw);
+    }
+}
